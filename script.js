e0426ebda491f8e6aff58bfb066aaf56d03cfb71
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 23:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //complete this code
-class Rectangle {}
-
-class Square extends Animal {}
+class Rectangle {
+	constructor(width,height);{
+		this.width=width;
+		this.height=height;
+}
+get width(){
+	this.width;
+}
+set height(){
+	this.height;
+}
+	getArea(){
+		return length*width;
+	}
+}
+class Square extends Rectangle {
+	getPerimeter(){
+		return 2*22/7*radius;
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
